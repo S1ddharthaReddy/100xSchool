c3513893bac914ca31d0e0843fca4a6117cd5daa
Siddhartha Reddy <siddharthareddychada@gmail.com>
2026-03-15 23:34:55 +0530

    node assignments

--- nodejs-assignments-master/01-nodejs/todoServer.js
+++ nodejs-assignments-master/01-nodejs/todoServer.js
@@ -45,5 +45,52 @@
   const app = express();
   
   app.use(bodyParser.json());
+
+  app.get("/todos", (req, res)=>{
+    res.json(todos);
+  })
+
+  app.get("/todos/:id", (req, res)=>{
+    const todo = todos.find(t => t.id === parseInt(req.params.id));
+    if(!todo) {
+      res.status(400).send();
+    }
+    else{
+      res.json(todo);
+    }
+  })
+
+  app.post("/todos", (req, res)=>{
+    const newTodo = {
+      id: Math.floor(Math.random()*100000),
+      title: req.body.title,
+      description: req.body.description
+    };
+    todos.push(newTodo);
+    res.status(201).json(newTodo);
+  })
+
+  app.put("todos/:id", (req, res)=>{
+    const todoIndex = todos.find(t => t.id === parseInt(req.params.id));
+    if(todoIndex === -1){
+      res.status(404).send();
+    }
+    else{
+      todos[todoIndex].title = req.body.title;
+      todos[todoIndex].description = req.body.description;
+      res.json(todos[todoIndex]);
+    }
+  })
+
+  app.delete("todos/:id", (req, res)=>{
+    const todoIndex = todos.find(t => t.id === parseInt(req.body.id));
+    if(todoIndex === -1){
+      res.sendStatus(404);
+    }
+    else{
+      todos.splice(todoIndex, 1);
+      res.status(200).send();
+    }
+  })
   
   module.exports = app;
