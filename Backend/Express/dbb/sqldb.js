3785ba41f9f5b8e5b02479671214309c918b9ef9
Siddhartha Reddy <siddharthareddychada@gmail.com>
2026-05-12 01:02:20 +0530

    sql in node

---
+++ Backend/Express/dbb/sqldb.js
@@ -0,0 +1,55 @@
+
+const express = require("express");
+const { Pool } = require("pg");
+
+const pool = new Pool({
+    connectionString: "postgresql://neondb_owner:npg_Pzd8uhpJam7R@ep-hidden-bar-api4v8kv.c-7.us-east-1.aws.neon.tech/neondb?sslmode=require"
+});
+
+const app = express();
+app.use(express.json());
+
+app.post("/signup", async (req, res) => {
+    const { username, email, password } = req.body;
+
+    // A very bad way to do SQL using PG --- This is vulnerable to SQL Injection
+    // const response = await pool.query(
+    //     `INSERT INTO users (username, email, password)
+    //      VALUES ('${username}', '${email}', '${password}') RETURNING id;`
+    //   );
+
+    // The best way to write this is
+    const response = await pool.query(
+        `INSERT INTO users (username, email, password)
+         VALUES ($1, $2, $3) RETURNING id;`, [username, email, password]
+      );
+
+    console.log(response);
+
+    res.json({
+        id: response.rows[0].id,
+        message: "Signup done"
+    })
+})
+
+app.post("/signin", async (req, res) => {
+    const { email, password } = req.body;
+
+    const response = await pool.query(`SELECT * FROM users WHERE email='${email}' AND password='${password}'`);
+    console.log(response);
+
+    const userExists = response.rows[0];
+
+    if(!userExists) {
+        res.status(403).json({
+            message: "incorrect creds"
+        })
+    }
+    else {
+        res.json({
+            token: "token"
+        })
+    }
+})
+
+app.listen(3000);
