172745ffd806bc95534868ef29ef915488d48afb
Siddhartha Reddy <siddharthareddychada@gmail.com>
2026-05-18 16:35:20 +0530

    chat app backend

---
+++ WebSockets/Chat-App/backend/dist/index.js
@@ -0,0 +1,30 @@
+import { WebSocketServer, WebSocket } from "ws";
+const wss = new WebSocketServer({ port: 8080 });
+let allSockets = [];
+wss.on("connection", (socket) => {
+    socket.on("message", (message) => {
+        // ts-ignore
+        const parsedMessage = JSON.parse(message);
+        if (parsedMessage.type === "join") {
+            allSockets.push({
+                socket,
+                room: parsedMessage.payload.roomId
+            });
+        }
+        if (parsedMessage.type === "chat") {
+            // const currentUserRoom = allSockets.find((x) => x.socket == socket)
+            let currentUserRoom = null;
+            for (let i = 0; i < allSockets.length; i++) {
+                if (allSockets[i]?.socket === socket) {
+                    currentUserRoom = allSockets[i]?.room;
+                }
+            }
+            for (let i = 0; i < allSockets.length; i++) {
+                if (allSockets[i]?.room == currentUserRoom) {
+                    allSockets[i]?.socket.send(parsedMessage.payload.message.toString());
+                }
+            }
+        }
+    });
+});
+//# sourceMappingURL=index.js.map
