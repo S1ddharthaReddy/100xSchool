65a14682788bc6a1a58806ed3bdd382ba675d3b2
Siddhartha Reddy <siddharthareddychada@gmail.com>
2026-05-21 11:45:33 +0530

    chat app done

---
+++ WebSockets/Chat-App/frontend/src/App.tsx
@@ -0,0 +1,57 @@
+
+import { useEffect, useRef, useState } from 'react'
+import './App.css'
+
+function App() {
+
+  const [messages, setMessages] = useState(["hi there", "hello"]);
+  const wsRef = useRef();
+
+  useEffect(() => {
+    const ws = new WebSocket("ws://localhost:8080");
+    ws.onmessage = (event) => {
+      setMessages(m => [...m, event.data])
+    }
+    wsRef.current = ws;
+
+    ws.onopen = () => {
+      ws.send(JSON.stringify({
+        type: "join",
+        payload: {
+          roomId: "red"
+        }
+      }))
+    }
+
+  }, [])
+
+  return (
+    <div className='h-screen bg-black'>
+      <br /> <br /> <br />
+      <div className='h-[95vh] bg-black'>
+        {messages.map(message => <div className='m-9'>
+           <span className='bg-white text-black rounded p-4 m-4'>
+             {message} 
+            </span> 
+           </div> )}
+      </div>
+      <div className='w-full bg-white flex'>
+        <input id="message" type="text" className='flex-1 p-4' />
+        <button onClick={()=>{
+          const message = document.getElementById("message")?.value;
+          wsRef.current.send(JSON.stringify({
+            type: "chat",
+            payload: {
+              message: message
+            }
+          }))
+        }}
+          className='bg-purple-600 text-white p-4'>
+          Send Message
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default App
